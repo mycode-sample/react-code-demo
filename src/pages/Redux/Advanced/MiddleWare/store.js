38026c5706a9a36fc720c98922c514f04d8eeec3
sheepfly <developer@noreply.email.com>
2020-10-24 19:23:03 +0800

    增加崩溃日志记录

--- src/pages/Redux/Advanced/MiddleWare/store.js
+++ src/pages/Redux/Advanced/MiddleWare/store.js
@@ -3,12 +3,29 @@ import calc from './reducers';
 
 const store = createStore(calc);
 
-const next = store.dispatch;
 
-store.dispatch = (actions) => {
-  console.log("before", store.getState());
-  next(actions);
-  console.log("after", store.getState());
-};
+function addLog(store) {
+  const next = store.dispatch;
+  store.dispatch = function(actions) {
+    console.log("before", store.getState());
+    next(actions);
+    console.log("after", store.getState());
+  };
+}
+
+function addCrash(store) {
+  const next = store.dispatch;
+  store.dispatch = function(actions) {
+    try {
+      return next(actions);
+    } catch (err) {
+      console.log("errors");
+      console.log(err);
+    }
+  };
+}
+
+addLog(store);
+addCrash(store);
 
 export default store;
