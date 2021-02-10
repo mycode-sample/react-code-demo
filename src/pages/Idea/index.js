a23fb5f5b3e590e404ef8bdaef51e48356f36f78
sheepfly <developer@noreply.email.com>
2021-02-11 01:25:01 +0800

    test

---
+++ src/pages/Idea/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Idea extends Component {
+    render() {
+        return(
+            <div>
+                hello.
+            </div>
+        )
+    }
+}
